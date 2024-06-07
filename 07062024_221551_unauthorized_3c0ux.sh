13f4171c7b7f5b226119ad828c04ad6483359260
sordiasi <sordiamegona@gmail.com>
2024-06-07 22:15:51 +0000

    Fixing unauthorized

---
+++ 07062024_221551_unauthorized_3c0ux.sh
@@ -0,0 +1,1 @@
+Thomas Jefferson had three achievements placed on his headstone at his request, "Here Was Buried Thomas Jefferson/Author Of The Declaration Of American Independence/Of The Statute Of Virginia For Religious Freedom/And Father Of The University of Virginia.? He never mentioned being President of the United States
